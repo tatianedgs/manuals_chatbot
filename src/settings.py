d0b8509dfc444cccca88310bf7f1e55269491213
tatianedgs <126398641+tatianedgs@users.noreply.github.com>
2025-08-20 18:07:06 -0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -10,6 +10,7 @@ class Settings:
     milvus_uri: str = os.getenv("MILVUS_URI", "http://localhost:19530")
     milvus_user: str = os.getenv("MILVUS_USER", "")
     milvus_password: str = os.getenv("MILVUS_PASSWORD", "")
+    milvus_token: str = os.getenv("MILVUS_TOKEN", "")  # << novo
     milvus_db: str = os.getenv("MILVUS_DB", "nupe")
     milvus_collection: str = os.getenv("MILVUS_COLLECTION", "docs_nupetr")
     ollama_host: str = os.getenv("OLLAMA_HOST", "http://localhost:11434")
